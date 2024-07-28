f45ad688dc7f10f6587d1a9a2cacdca5b5c7ce43
snow4rider <matthew.t.mckenney@gmail.com>
2024-07-27 20:26:33 -0600

    adding cmake

---
+++ shapes_prac/build/CMakeFiles/Shapes.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shapes.dir/main.cpp.o"
+  "CMakeFiles/Shapes.dir/main.cpp.o.d"
+  "Shapes"
+  "Shapes.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shapes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

c1ef38dd4f7ddedd365396c1e914cc36fca004bf
snow4rider <matthew.t.mckenney@gmail.com>
2024-07-31 22:37:18 -0600

    working through chapter 1

---
+++ Chapter_01/exercise_1-1/exercise_1-1.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int abslute_value(int x) {
+  // code goes here
+  return 0;
+}
+
+int main() {
+  int my_num = -10;
+
+  printf("The absolute value of %d is %d.\n", my_num, abslute_value(my_num));
+}

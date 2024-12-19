30d965bbefcdf6f81209233dcf2153942cabaeda
snow4rider <matthew.t.mckenney@gmail.com>
2024-12-18 22:17:26 -0700

    working on chapter 7

--- Chapter_07/listing_7-10/main.cpp
+++ Chapter_07/listing_7-10/main.cpp
@@ -1,11 +1,21 @@
 #include <cstdio>
 
+/// @brief Print the address of a void pointer
+///
+/// This function takes a void pointer and prints its address in hexadecimal
+/// format to the standard output.
 void
 print_addr (void *x)
 {
   printf ("0x%p\n", x);
 }
 
+/**
+ * @brief Main entry point of the program.
+ *
+ * This function demonstrates the use of the print_addr function by printing
+ * the address of an integer variable and a null pointer in hexadecimal format.
+ */
 int
 main ()
 {
